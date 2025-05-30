f35e877a4b46c15dae80657db6f54e2352d7c79b
Nish-mitha <nishmithasullal@gmail.com>
2025-05-30 22:02:28 +0530

    Day 3

---
+++ Day-003-challenge.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int findLengthOfLastWord(string str) {
+    int counter = 0;
+    bool foundChar = false;
+    for(int i = str.length() - 1; i >= 0; i--) {
+        if(str[i] == ' ' && foundChar) {
+            return counter;
+        } else if(str[i] != ' ') {
+            foundChar = true;
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    cout << findLengthOfLastWord(str) << endl;
+    return 0;
+}
